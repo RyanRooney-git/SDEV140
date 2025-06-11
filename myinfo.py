2ecabe824108e14601b7189eff7a6c0913c1b279
Ryan Rooney <145315125+RyanRooney-git@users.noreply.github.com>
2025-06-11 14:22:34 -0400

    Week 1 assignment

---
+++ myinfo.py
@@ -0,0 +1,11 @@
+"""
+Author:  Ryan Rooney
+Date written: 6/9/2025
+Assignment:   Module 01 exercise 1 part 1.
+Short Desc:   In this assignment I will write a Python program in a file named myinfo.py 
+that prints my name, address and telephone number.
+
+"""
+print('Adam Levey') # Displays name
+print('123456 Dust Road') # Displays address input
+print('123-545-6780') # Displays phone number
